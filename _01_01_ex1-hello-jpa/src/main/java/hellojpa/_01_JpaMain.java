13ab3806d1a79a94d62bb6c405336541eade101e
sakku <kws1044@gmail.com>
2023-01-29 14:38:58 +0900

    클래스명 변경

--- _01_01_ex1-hello-jpa/src/main/java/hellojpa/_01_JpaMain.java
+++ _01_01_ex1-hello-jpa/src/main/java/hellojpa/_01_JpaMain.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class JpaMain {
+public class _01_JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
--- _01_01_ex1-hello-jpa/src/main/java/hellojpa/_02_JpaMain02.java
+++ _01_01_ex1-hello-jpa/src/main/java/hellojpa/_02_JpaMain02.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
 
-public class JpaMain02 {
+public class _02_JpaMain02 {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
