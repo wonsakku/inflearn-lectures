157fff8dd2dd1eb5fcda1faa91921c83b7eeab1a
sakku <kws1044@gmail.com>
2023-03-04 11:12:00 +0900

    data jpa project init

---
+++ _01_05_data-jpa/src/test/java/org/inflearn/DataJpaApplicationTest.java
@@ -0,0 +1,13 @@
+package org.inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DataJpaApplicationTest {
+
+    @Test
+    void test(){
+
+    }
+}
