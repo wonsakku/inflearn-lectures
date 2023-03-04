4353fc532712284af7822405e1106ff54b3d336b
sakku <kws1044@gmail.com>
2023-03-04 12:00:44 +0900

    스프링_데이터_JPA_섹션2. 예제 도메인 모델

--- _01_05_data-jpa/src/main/java/org/inflearn/entity/Member.java
+++ _01_05_data-jpa/src/main/java/org/inflearn/entity/Member.java
@@ -2,24 +2,44 @@ package org.inflearn.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
     public void changeUsername(String username){
         this.username = username;
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        this.team.getMembers().add(this);
     }
 }
