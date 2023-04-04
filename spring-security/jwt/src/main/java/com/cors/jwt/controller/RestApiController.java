365a6e6f8d2b1367e32c6fbf2fbfc099aba570ae
wonsakku <kws1044@gmail.com>
2023-04-04 09:46:01 +0900

    jwt project init

---
+++ spring-security/jwt/src/main/java/com/cors/jwt/controller/RestApiController.java
@@ -0,0 +1,16 @@
+package com.cors.jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApiController {
+
+
+    @GetMapping("/home")
+    public String home(){
+        return "<h1>HOME</h1>";
+    }
+
+
+}
