83bf882643e61014891ede9b1ffb986653ca878e
wonsakku <kws1044@gmail.com>
2023-04-10 10:49:06 +0900

    querydsl project init

---
+++ jpa-kyh/_01_06_querydsl/src/main/java/study/_01_06_querydsl/controller/HelloController.java
@@ -0,0 +1,14 @@
+package study._01_06_querydsl.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+    
+}
